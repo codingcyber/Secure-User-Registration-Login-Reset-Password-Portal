55c20cd53f3baddaba2ba41b7b1bc1307408e3d0
codingcyber <vivek@codingcyber.com>
2019-06-28 00:37:23 +0530

    Sending Account Activation Confirmation Email & Adding to Activity Log

--- activate.php
+++ activate.php
@@ -31,10 +31,51 @@ if($count == 1){
         // delete activation key record from user_active table
         $delsql = "DELETE FROM user_active WHERE active_token=?";
         $delresult = $db->prepare($delsql);
-        $delresult->execute(array($_GET['key'])); 
+        $delresult->execute(array($_GET['key']));
+        $messages[] = 'Preparing Your Account for First Time Login';
+        // adding activity in user_activity table
+        $actsql = "INSERT INTO user_activity (uid, activity) VALUES (:uid, :activity)";
+        $actresult = $db->prepare($actsql);
+        $values = array(':uid'          => $_GET['id'],
+                        ':activity'     => 'User Registered'
+                        );
+        $actresult->execute($values);
+        $messages[] = 'Adding User Registration Log Entry';
+        
+        // send confirmation email to user
+        $mail = new PHPMailer(true);
+
+        try {
+            //Server settings
+            $mail->isSMTP();                                            // Set mailer to use SMTP
+            $mail->Host       = $smtphost;  // Specify main and backup SMTP servers
+            $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+            $mail->Username   = $smtpuser;                     // SMTP username
+            $mail->Password   = $smtppass;                               // SMTP password
+            $mail->SMTPSecure = 'tls';                                  // Enable TLS encryption, `ssl` also accepted
+            $mail->Port       = 587;                                    // TCP port to connect to
+
+            //Recipients
+            $mail->setFrom('test@example.com', 'Vivek Vengala');
+            // update recipient email with dynamic email
+            $mail->addAddress('vivek@codingcyber.com', 'Vivek Vengala');     // Add a recipient
+
+            // Content
+            $mail->isHTML(true);                                  // Set email format to HTML
+            $mail->Subject = 'Account Activated';
+            $mail->Body    = "Your Account Activated, Please Login</b>";
+            $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+            $mail->send();
+            $messages[] = 'Activation Email Sent, Follow the Instructions';
+        } catch (Exception $e) {
+            echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+        }
+
     }
+}else{
+    $errors[] = "Failed to Activate Your Account, Check with Site Admin!";
 }
-// send confirmation email to user
 ?>
 <div class="row">
     <div class="col-md-4 col-md-offset-4">
