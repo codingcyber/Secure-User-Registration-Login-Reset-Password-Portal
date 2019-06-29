b93697b9887636635689d546515cd3beae82875c
codingcyber <vivek@codingcyber.com>
2019-06-29 19:45:57 +0530

    Basic Login Functionality

--- login.php
+++ login.php
@@ -1,6 +1,24 @@
 <?php 
 include('includes/header.php'); 
 require_once('includes/connect.php');
+if(isset($_POST) & !empty($_POST)){
+    print_r($_POST);
+    // select sql query to check the email id in database
+    $sql = "SELECT * FROM users WHERE email=?";
+    $result = $db->prepare($sql);
+    $result->execute(array($_POST['email']));
+    $count = $result->rowCount();
+    $res = $result->fetch(PDO::FETCH_ASSOC);
+    if($count == 1){
+        $messages[] = "E-Mail already exists in database";
+        // then comparing the password with password hash
+        if(password_verify($_POST['password'], $res['password'])){
+            $messages[] = "Create Session and Redirect user to Members Area";
+        }else{
+            $errors[] = "E-Mail / Password Combination not Working";
+        }
+    }
+}
 ?>
 <div class="row">
     <div class="col-md-4 col-md-offset-4">
@@ -9,7 +27,25 @@ require_once('includes/connect.php');
                 <h3 class="panel-title">Please Sign In</h3>
             </div>
             <div class="panel-body">
-                <form role="form">
+                <?php
+                    if(!empty($messages)){
+                        echo "<div class='alert alert-success'>";
+                        foreach ($messages as $message) {
+                            echo "<span class='glyphicon glyphicon-ok'></span>&nbsp;". $message ."<br>";
+                        }
+                        echo "</div>";
+                    }
+                ?>
+                <?php
+                    if(!empty($errors)){
+                        echo "<div class='alert alert-danger'>";
+                        foreach ($errors as $error) {
+                            echo "<span class='glyphicon glyphicon-remove'></span>&nbsp;". $error ."<br>";
+                        }
+                        echo "</div>";
+                    }
+                ?>
+                <form role="form" method="post">
                     <fieldset>
                         <div class="form-group">
                             <input class="form-control" placeholder="E-mail" name="email" type="email" autofocus>
@@ -23,7 +59,7 @@ require_once('includes/connect.php');
                             </label>
                         </div>
                         <!-- Change this to a button or input when using this as a form -->
-                        <a href="index.html" class="btn btn-lg btn-success btn-block">Login</a>
+                        <input type="submit" class="btn btn-lg btn-success btn-block" value="Login" />
                     </fieldset>
                 </form>
             </div>
