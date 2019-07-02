65412b494664d310a8774cd37a0f21c4dfc1c19a
Vivek Vengala <vivek@codingcyber.com>
2019-07-02 23:29:35 +0530

    Limiting Failed Login Attempts - Brute Force Attacks

--- login.php
+++ login.php
@@ -2,6 +2,7 @@
 session_start();
 include('includes/header.php'); 
 require_once('includes/connect.php');
+$failmax = 5;
 if(isset($_POST) & !empty($_POST)){
     if(empty($_POST['email'])){ $errors[] = 'User Name / E-mail field is Required';}
     if(empty($_POST['password'])){ $errors[] = 'Password field is Required';}
@@ -45,37 +46,55 @@ if(isset($_POST) & !empty($_POST)){
         $count = $result->rowCount();
         $res = $result->fetch(PDO::FETCH_ASSOC);
         if($count == 1){
-            // then comparing the password with password hash
-            if(password_verify($_POST['password'], $res['password'])){
-                $messages[] = "Create Session and Redirect user to Members Area";
-                // Insert Activity into DB Table - user_activity
-                $actsql = "INSERT INTO user_activity (uid, activity) VALUES (:uid, :activity)";
-                $actresult = $db->prepare($actsql);
-                $values = array(':uid'          => $res['id'],
-                                ':activity'     => 'User LoggedIn'
-                                );
-                $actresult->execute($values);
+            // Checking number of failed login attempts
+            $failsql = "SELECT * FROM login_fail WHERE uid=? AND loginfailed > NOW() - INTERVAL 60 MINUTE";
+            $failresult = $db->prepare($failsql);
+            $failresult->execute(array($res['id']));
+            $failcount = $failresult->rowCount();
+            if($failcount < $failmax){
+                $errors[] = 'Number of Failed Attempts' . $failcount;
+                // we will check the password & create the session here
+                // then comparing the password with password hash
+                if(password_verify($_POST['password'], $res['password'])){
+                    $messages[] = "Create Session and Redirect user to Members Area";
+                    // Insert Activity into DB Table - user_activity
+                    $actsql = "INSERT INTO user_activity (uid, activity) VALUES (:uid, :activity)";
+                    $actresult = $db->prepare($actsql);
+                    $values = array(':uid'          => $res['id'],
+                                    ':activity'     => 'User LoggedIn'
+                                    );
+                    $actresult->execute($values);
 
-                // Insert Login timestamps into DB Table - login_log
-                $loginsql = "INSERT INTO login_log (uid, loggedin) VALUES (:uid, NOW())";
-                $loginresult = $db->prepare($loginsql);
-                $values = array(':uid'          => $res['id'] );
-                $loginresult->execute($values);
+                    // Insert Login timestamps into DB Table - login_log
+                    $loginsql = "INSERT INTO login_log (uid, loggedin) VALUES (:uid, NOW())";
+                    $loginresult = $db->prepare($loginsql);
+                    $values = array(':uid'          => $res['id'] );
+                    $loginresult->execute($values);
+                }else{
+                    $actsql = "INSERT INTO user_activity (uid, activity) VALUES (:uid, :activity)";
+                    $actresult = $db->prepare($actsql);
+                    $values = array(':uid'          => $res['id'],
+                                    ':activity'     => 'User LogIn Failed'
+                                    );
+                    $actresult->execute($values);
+
+                    // Insert Failed Login timestamp in login_fail table
+                    $loginfailsql = "INSERT INTO login_fail (uid) VALUES (:uid)";
+                    $loginfailresult = $db->prepare($loginfailsql);
+                    $values = array(':uid'          => $res['id'] );
+                    $loginfailresult->execute($values);
+
+                    // calculate the number of remaining attempts
+                    $remainingattempts = $failmax - $failcount;
+
+                    $errors[] = "Invalid User Name / E-Mail & Password Combination";
+                    $errors[] = "You have {$remainingattempts} login attempts remaining, otherwise you will be blocked for 60 minutes";
+                }
             }else{
-                $errors[] = "User Name / E-Mail & Password Combination not Working";
-                $actsql = "INSERT INTO user_activity (uid, activity) VALUES (:uid, :activity)";
-                $actresult = $db->prepare($actsql);
-                $values = array(':uid'          => $res['id'],
-                                ':activity'     => 'User LogIn Failed'
-                                );
-                $actresult->execute($values);
-
-                // Insert Failed Login timestamp in login_fail table
-                $loginfailsql = "INSERT INTO login_fail (uid) VALUES (:uid)";
-                $loginfailresult = $db->prepare($loginfailsql);
-                $values = array(':uid'          => $res['id'] );
-                $loginfailresult->execute($values);
+                $errors[] = 'You are blocked for 60 minutes to login, retry after some time';
             }
+
+            
         }else{
             $errors[] = "User Name / E-Mail Not Valid";
             // Insert Failed Login Attempt to user_activity table
