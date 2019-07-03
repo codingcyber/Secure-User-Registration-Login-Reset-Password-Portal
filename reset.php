1d4a30e0940be21ff2f7c6a85545cc8b8cc6bcb9
codingcyber <vivek@codingcyber.com>
2019-07-03 23:45:39 +0530

    Preparing Reset page for Reset Functionality

--- reset.php
+++ reset.php
@@ -1,6 +1,43 @@
 <?php 
+session_start();
 include('includes/header.php'); 
 require_once('includes/connect.php');
+if(isset($_POST) & !empty($_POST)){
+    if(empty($_POST['email'])){ $errors[] = 'User Name field is Required';}
+
+    // CSRF Token Validation
+    if(isset($_POST['csrf_token'])){
+        if($_POST['csrf_token'] === $_SESSION['csrf_token']){
+        }else{
+            $errors[] = "Problem with CSRF Token Verification";
+        }
+    }else{
+        $errors[] = "Problem with CSRF Token Validation";
+    }
+
+    // CSRF Token Time Validation
+    $max_time = 60*60*24;
+    if(isset($_SESSION['csrf_token_time'])){
+        $token_time = $_SESSION['csrf_token_time'];
+        if(($token_time + $max_time) >= time()){
+        }else{
+            $errors[] = "CSRF Token Expired";
+            unset($_SESSION['csrf_token']);
+            unset($_SESSION['csrf_token_time']);
+        }
+    }else{
+        unset($_SESSION['csrf_token']);
+        unset($_SESSION['csrf_token_time']);
+    }
+
+    if(empty($errors)){
+        $messages[] = 'Check User Name / Email exists in Database, if exists create reset token and send email';
+    }
+}   
+// Create CSRF token
+$token = md5(uniqid(rand(), TRUE));
+$_SESSION['csrf_token'] = $token;
+$_SESSION['csrf_token_time'] = time();
 ?>
 <div class="row">
     <div class="col-md-4 col-md-offset-4">
@@ -9,13 +46,32 @@ require_once('includes/connect.php');
                 <h3 class="panel-title">Reset Password</h3>
             </div>
             <div class="panel-body">
+                <?php
+                    if(!empty($messages)){
+                        echo "<div class='alert alert-success'>";
+                        foreach ($messages as $message) {
+                            echo "<span class='glyphicon glyphicon-ok'></span>&nbsp;". $message ."<br>";
+                        }
+                        echo "</div>";
+                    }
+                ?>
+                <?php
+                    if(!empty($errors)){
+                        echo "<div class='alert alert-danger'>";
+                        foreach ($errors as $error) {
+                            echo "<span class='glyphicon glyphicon-remove'></span>&nbsp;". $error ."<br>";
+                        }
+                        echo "</div>";
+                    }
+                ?>
                 <form role="form" method="post">
+                    <input type="hidden" name="csrf_token" value="<?php echo $token; ?>">
                     <fieldset>
                         <div class="form-group">
-                            <input class="form-control" placeholder="E-mail" name="email" type="email" autofocus>
+                            <input class="form-control" placeholder="E-mail or User Name" name="email" type="text" autofocus value="<?php if(isset($_POST['uname'])){ echo $_POST['uname']; } ?>">
                         </div>
                         <!-- Change this to a button or input when using this as a form -->
-                        <a href="index.html" class="btn btn-lg btn-success btn-block">Reset Password</a>
+                        <input type="submit" class="btn btn-lg btn-success btn-block" value="Reset Password" />
                     </fieldset>
                 </form>
             </div>
